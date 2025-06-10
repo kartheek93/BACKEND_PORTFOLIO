e4b7eb378721368d6714d0951d477e2b0ab8b381
kartheek93 <vkartheek0000@gmail.com>
2025-06-10 10:55:15 +0530

    First commit with project files

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "postgresql://postgres:938186@localhost/portfolio"
+        "postgresql://postgres:938186@localhost:5432/portfolio"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
