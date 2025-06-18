df8500818ef847d8a8bea4f115a1c7b987408c7d
kartheek <146115095+kartheek93@users.noreply.github.com>
2025-06-18 21:10:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://backend_portfolio_eyt8_user:rRLsWfP17GnYFdXQPJzwhXfm3QOrO1VW@dpg-d13sc224d50c73e9rktg-a/backend_portfolio_eyt8"
+        "postgresql://postgres:zwMDVQfHvWzxjSOWivLAhzezbmsOzqZm@interchange.proxy.rlwy.net:37049/railway"
+        #"postgresql://backend_portfolio_eyt8_user:rRLsWfP17GnYFdXQPJzwhXfm3QOrO1VW@dpg-d13sc224d50c73e9rktg-a/backend_portfolio_eyt8"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
